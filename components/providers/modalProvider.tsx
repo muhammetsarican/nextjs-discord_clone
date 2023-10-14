5fee31c3efba1c8175a27ec5a53e9c0f295e2a17
Muhammet <muhammetsarican@gmail.com>
2023-10-14 17:20:24 +0300

    server settings operations completed.

--- components/providers/modalProvider.tsx
+++ components/providers/modalProvider.tsx
@@ -2,6 +2,7 @@
 import { CreateServerModal } from "@/components/modals/createServerModal";
 import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/inviteModal";
+import { EditServerModal } from "@/components/modals/editServerModal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -14,6 +15,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
