c9c9a685e7a04c6e33529f614daf83e984fc180c
Muhammet <muhammetsarican@gmail.com>
2023-10-17 22:31:40 +0300

    channel create operations completed.

--- components/providers/modalProvider.tsx
+++ components/providers/modalProvider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/inviteModal";
 import { EditServerModal } from "@/components/modals/editServerModal";
 import { MembersModal } from "../modals/membersModal";
+import { CreateChannelModal } from "../modals/createChannelModal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
