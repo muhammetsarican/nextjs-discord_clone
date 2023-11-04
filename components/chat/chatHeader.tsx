a05ac3e2d72d5278077b65820ddc97c52d471ff1
Muhammet Sarıcan <muhammetsarican@gmail.com>
2023-11-04 23:02:40 +0300

    The conversation part of the chat header is completed.

--- components/chat/chatHeader.tsx
+++ components/chat/chatHeader.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobileToggle";
+import { UserAvatar } from "../userAvatar";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -16,13 +17,16 @@ const ChatHeader = ({
 }: ChatHeaderProps) => {
     return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-            <MobileToggle serverId={serverId}/>
-            {type ==="channel" && (
-                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
-                )}
-                <p className="font-semibold text-md text-black dark:text-white">
-                    {name}
-                </p>
+            <MobileToggle serverId={serverId} />
+            {type === "channel" && (
+                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+            )}
+            {type === "conversation" && (
+                <UserAvatar src={imageUrl} className="h-8 w-8 md:w-8 md:h-8 mr-2" />
+            )}
+            <p className="font-semibold text-md text-black dark:text-white">
+                {name}
+            </p>
         </div>
     )
 }
