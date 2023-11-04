059ac79851d72fc20351cde2e48d1105c5fd64f7
Muhammet Sarıcan <muhammetsarican@gmail.com>
2023-11-04 17:07:25 +0300

    chat header created but partially completed.

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,45 +1,52 @@
-import { currentProfile } from "@/lib/currentProfile";
-import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-interface ServerIdPageProps{
-    params:{
-        serverId:string;
-    }
-}
-const ServerIdPage=async({params}:ServerIdPageProps)=>{
-    const profile = await currentProfile();
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
 
-    if(!profile) return redirectToSignIn();
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  }
+};
 
-    const server=await db.server.findUnique({
-        where:{
-            id:params.serverId,
-            members:{
-                some:{
-                    profileId:profile.id,
-                }
-            }
-        },
-        include:{
-            channels:{
-                where:{
-                    name:"general",
-                },
-                orderBy:{
-                    createdAt:"asc"
-                }
-            }
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
         }
-    })
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general"
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }
+    }
+  })
 
-    const initialChannel=server?.channels[0];
+  const initialChannel = server?.channels[0];
 
-    if(initialChannel?.name !== "general") return null;
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
 
-    
-    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
-
+ 
 export default ServerIdPage;
